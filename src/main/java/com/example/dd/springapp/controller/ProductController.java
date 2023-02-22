9933abf9cfa69729e630ea28aa7c6624c2c3e779
Kamidro6530 <daniel6530@wp.pl>
2023-02-22 08:59:13 +0100

    Set  list limit per page for 3

--- src/main/java/com/example/dd/springapp/controller/ProductController.java
+++ src/main/java/com/example/dd/springapp/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.example.dd.springapp.controller;
 
 import com.example.dd.springapp.model.product.ProductRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
 public class ProductController {
@@ -10,5 +15,15 @@ public class ProductController {
 
      ProductController(final ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @GetMapping(value = "/products",params = {"!sort","!page","!size"})
+    ResponseEntity<?> readAllProducts(){
+        return ResponseEntity.ok(productRepository.findAll(PageRequest.ofSize(3)));
+    }
+
+    @GetMapping(value = "/products")
+    ResponseEntity<?> readAllProductsPageable(Pageable page){
+         return ResponseEntity.ok(productRepository.findAll(PageRequest.of(page.getPageNumber(),3)));
     }
 }
